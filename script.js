51a5b4f24632bc436f85fe6c9527d59827e7608f
Miguel de Leon <miguelangelodeleon.12@gmail.com>
2022-09-03 19:28:51 +0200

    feat: added more informational

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 const moveSet = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
+
 const computerPlay = () => moveSet[Math.floor(Math.random() * moveSet.length)];
 
 const playerPlay = () => {
-  let playerMove = prompt('type: rock, paper, scissors').toLowerCase();
+  let playerMove = prompt(
+    'type: rock, paper, scissors. Wrong spelling will chose rock'
+  ).toLowerCase();
   //checks if there is a valid move
   const playerMoveCheck = moveSet.some((move) => move === playerMove);
-  // playerMoveCheck ? playerMove : playerPlay('please check your spelling');
-  if (playerMoveCheck) {
-    return playerMove;
-  }
-  return 'rock';
+  return playerMoveCheck ? playerMove : 'rock';
 };
-
-let playerSelection;
-let computerSelection;
-let playerScore = 0;
-let computerScore = 0;
 
 const play = (playerSelection, computerSelection) => {
   // //get the first letter of both computer and player
@@ -26,18 +22,20 @@ const play = (playerSelection, computerSelection) => {
     case 'pr':
     case 'sp':
       playerScore++;
-      return `you win!\n your score: ${playerScore} \n computer score: ${computerScore}`;
+      return `you win!\n your choice: ${playerSelection} \n computer's choice: ${computerSelection} \n your score: ${playerScore} \n computer score: ${computerScore}`;
     case 'sr':
     case 'rp':
     case 'ps':
       computerScore++;
-      return `you lose! \n your score: ${playerScore} \n computer score: ${computerScore}`;
+      return `you lose! \n your choice: ${playerSelection} \n computer's choice: ${computerSelection} \n your score: ${playerScore} \n computer score: ${computerScore}`;
     default:
-      return `draw \n your score: ${playerScore} \n computer score: ${computerScore}`;
+      return `draw \n your choice: ${playerSelection} \n computer's choice: ${computerSelection} \n your score: ${playerScore} \n computer score: ${computerScore}`;
   }
 };
 
 const getGame = () => {
+  let playerSelection;
+  let computerSelection;
   for (let i = 0; i < 5; i++) {
     playerSelection = playerPlay();
     computerSelection = computerPlay();
